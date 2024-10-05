5336044828b37967b56d9b358aef2322385f11c6
Bastien <bastien.pouessel@epita.fr>
2024-10-05 11:54:44 -0700

    feat: add simple chabot with fireworks

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import streamlit as st
+from dotenv import load_dotenv
+from llama_index.llms.fireworks import Fireworks
+import os
+import asyncio
+
+# Load environment variables
+load_dotenv()
 
 
-if __name__ == "__main__":
+FIREWORK_API_KEY = os.getenv("FIREWORK_API_KEY")
+
+
+async def build_ui(agent):
     st.title("CyberGuard")
 
     if "messages" not in st.session_state:
@@ -15,15 +25,30 @@ if __name__ == "__main__":
         st.chat_message("user").markdown(prompt)
         st.session_state.messages.append({"role": "user", "content": prompt})
 
-        response = f"Echo: {prompt}"
+        # Use the LLM to generate a response
+
+
+        
+        response = await agent.acomplete(prompt)
+
         with st.chat_message("assistant"):
-            st.markdown(
-                """
-                ```python
-                a = 1
-                b = 2
-                print(a + b)
-                ```
-            """
-            )
+            st.markdown(response)
         st.session_state.messages.append({"role": "assistant", "content": response})
+
+
+async def create_agent():
+    llm = Fireworks(
+        model="accounts/fireworks/models/llama-v3p2-1b-instruct",
+        api_key=FIREWORK_API_KEY,
+    )
+
+    return llm
+
+
+async def main():
+    llm = await create_agent()
+    await build_ui(llm)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
