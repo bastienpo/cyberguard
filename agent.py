7e7d3ebc3aaf78b422f2c5f48d08d841dae5bc64
Bastien <bastien.pouessel@epita.fr>
2024-10-05 17:01:12 -0700

    feat: replace a React with agent tree search

--- agent.py
+++ agent.py
@@ -9,6 +9,8 @@ from llama_index.core import VectorStoreIndex
 from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.vector_stores.qdrant import QdrantVectorStore
 from llama_index.embeddings.fastembed import FastEmbedEmbedding
+from llama_index.agent.lats import LATSAgentWorker
+from llama_index.core.agent import AgentRunner
 from llama_index.core.workflow import (
     StartEvent,
     StopEvent,
@@ -37,6 +39,48 @@ Settings.llm = Fireworks(
     max_tokens=4096,
     temperature=0.8,
 )
+
+vulnerability_template = """You are a cybersecurity expert. Analyze the following code and identify potential vulnerabilities. 
+
+For each vulnerability, provide a description and a code snippet that demonstrates the vulnerability.
+
+Format of the output:
+- SQL Injection : SQL Injection is a vulnerability that allows an attacker to inject SQL code into a database query.
+- Cross-Site Scripting (XSS) : XSS is a vulnerability that allows an attacker to inject JavaScript code into a web page.
+        
+Ensure each vulnerability is on a separate line. Be concise but thorough in your analysis and keep it to one line. If no vulnerabilities are found, state "No vulnerabilities detected."
+        
+User's code:
+{query}
+        
+Analyze the code above and **list only** the vulnerabilities in the specified format.
+"""
+
+fix_template = """Fix the code vulnerabilities and return **only** the fixed code.
+
+Retrieve information about vulnerabilities.
+
+Run the code and to ensure that the code is working.
+
+Identified vulnerabilities:
+{vulnerabilities}
+
+User's code:
+{query}
+"""
+
+test_template = """You are given a code snippet and a list of vulnerabilities.
+
+Generate and run test cases to verify that the fixed code works and fixes the vulnerabilities.
+
+Fixed code:
+{fixed_code}
+
+Vulnerabilities:
+{vulnerabilities}
+
+Run the test case and output the results of the test cases in markdown table format with OK if the test case passes or KO if it fails or unable to run and the code that was run.
+"""
 
 
 def code_interpret_tool(code: str):
@@ -121,25 +165,14 @@ class CyberWorkflow(Workflow):
         [code_tool, rag_tool], llm=llm, verbose=True, max_iterations=25
     )
 
+    agent_worker = LATSAgentWorker(
+        tools=[code_tool, rag_tool], num_expansions=3, max_rollouts=5, llm=llm
+    )
+
     @step
     async def identify_vulnerabilities(self, event: StartEvent) -> VulnerabilityEvent:
         """
         Identify potential vulnerabilities in the code.
-        """
-        vulnerability_template = """You are a cybersecurity expert. Analyze the following code and identify potential vulnerabilities. 
-
-        For each vulnerability, provide a description and a code snippet that demonstrates the vulnerability.
-
-        Format of the output:
-        - SQL Injection : SQL Injection is a vulnerability that allows an attacker to inject SQL code into a database query.
-        - Cross-Site Scripting (XSS) : XSS is a vulnerability that allows an attacker to inject JavaScript code into a web page.
-        
-        Ensure each vulnerability is on a separate line. Be concise but thorough in your analysis and keep it to one line. If no vulnerabilities are found, state "No vulnerabilities detected."
-        
-        User's code:
-        {query}
-        
-        Analyze the code above and **list only** the vulnerabilities in the specified format.
         """
 
         vulnerability_prompt = PromptTemplate(vulnerability_template)
@@ -156,19 +189,6 @@ class CyberWorkflow(Workflow):
     async def fix_vulnerabilities(
         self, event: VulnerabilityEvent
     ) -> FixVulnerabilityEvent:
-        fix_template = """Fix the code vulnerabilities and return **only** the fixed code.
-
-        Retrieve information about vulnerabilities.
-
-        Run the code and to ensure that the code is working.
-
-        Identified vulnerabilities:
-        {vulnerabilities}
-
-        User's code:
-        {query}
-        """
-
         fix_prompt = PromptTemplate(fix_template)
 
         fixed_code = await self.react_agent.achat(
@@ -185,22 +205,11 @@ class CyberWorkflow(Workflow):
     async def test_agent(self, event: FixVulnerabilityEvent) -> SummaryEvent:
         """Generate test cases to verify the fix."""
 
-        test_template = """You are given a code snippet and a list of vulnerabilities.
-
-        Generate and run test cases to verify that the fixed code works and fixes the vulnerabilities.
-
-        Fixed code:
-        {fixed_code}
-
-        Vulnerabilities:
-        {vulnerabilities}
-
-        Run the test case and output the results of the test cases in markdown table format with OK if the test case passes or KO if it fails or unable to run and the code that was run.
-        """
-
         test_prompt = PromptTemplate(test_template)
 
-        test_results = await self.react_agent.achat(
+        lats_agent = AgentRunner(self.agent_worker)
+
+        test_results = await lats_agent.achat(
             test_prompt.format(
                 fixed_code=event.fixed_code, vulnerabilities=event.vulnerabilities
             )
@@ -234,7 +243,7 @@ class CyberWorkflow(Workflow):
         {fixed_code}
 
         ## 3. Test results
-        (A markdown table of test cases you ran to verify the fix.)
+        (A markdown table of test cases with code, results and name of the test case.)
         
         Test results:
         {test_results}
